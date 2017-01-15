72151bd3fe63054900ab1a520866d3e199dc8b41
Ven Korolev <mohtaiiihuk@gmail.com>
2017-01-15 03:03:13 +0300

    first lesson

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: 'public',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ],
+  },
+};
