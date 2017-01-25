5cce230750c8035bb423d7b2c98769190f261b7a
Ven Korolev <mohtaiiihuk@gmail.com>
2017-01-25 04:32:32 +0300

    second lesson: webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Test from './test';
+import Test2 from './test2';
 
 render(
   <div>
     Hello react!
+    <Test />
+    <Test2 />
   </div>,
   document.getElementById('root')
 );
---
+++ src/test2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Test2 extends Component {
+  render() {
+    return (
+      <div>
+        Test2
+        <input type="text" />
+      </div>
+    )
+  }
+}
